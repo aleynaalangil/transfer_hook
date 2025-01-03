30fc91bb6bc53531bd3985be7898e1b70010e2d4
aleynaalangil <aleynaalangil@gmail.com>
2025-01-03 03:55:24 +0300

    removing from whitelist and wallet associated token acc matches

--- programs/transfer_hook/src/lib.rs
+++ programs/transfer_hook/src/lib.rs
@@ -16,12 +16,16 @@ use spl_tlv_account_resolution::{
 };
 use spl_transfer_hook_interface::instruction::ExecuteInstruction;
 
-declare_id!("7evqQb556TKMSHnKFLbsGQvTDceXNGGdS8yPDaBA4Hep");
+declare_id!("EGqyf8VoRxvyoy7Fxtowu6UAp83CZgNFuks6DsNHr17G");
 
 #[error_code]
 pub enum TransferError {
     #[msg("The token is not currently transferring")]
     IsNotCurrentlyTransferring,
+    #[msg("Only the whitelist authority can modify the whitelist")]
+    NotWhitelistAuthority,
+    #[msg("This (token_account, wallet_account) pair is already whitelisted")]
+    WalletAlreadyWhitelisted,
 }
 
 #[program]
@@ -46,45 +50,139 @@ pub mod transfer_hook {
         Ok(())
     }
 
+    // #[interface(spl_transfer_hook_interface::execute)]
+    // pub fn transfer_hook(ctx: Context<TransferHook>, _amount: u64) -> Result<()> {
+    //     // Fail this instruction if it is not called from within a transfer hook
+    //     check_is_transferring(&ctx)?;
+
+    //     if !ctx
+    //         .accounts
+    //         .white_list
+    //         .white_list
+    //         .contains(&ctx.accounts.destination_token.key())
+    //     {
+    //         panic!("Account not in white list!");
+    //     }
+
+    //     msg!("Account in white list, all good!");
+
+    //     Ok(())
+    // }
     #[interface(spl_transfer_hook_interface::execute)]
     pub fn transfer_hook(ctx: Context<TransferHook>, _amount: u64) -> Result<()> {
         // Fail this instruction if it is not called from within a transfer hook
         check_is_transferring(&ctx)?;
 
-        if !ctx
+        let destination_token_key = ctx.accounts.destination_token.key();
+        let owner_wallet_key = ctx.accounts.owner.key();
+
+        // We check if (destination token) or (owner wallet) is whitelisted
+        let is_whitelisted = ctx
             .accounts
             .white_list
             .white_list
-            .contains(&ctx.accounts.destination_token.key())
-        {
-            panic!("Account not in white list!");
+            .iter()
+            .any(|entry| {
+                // If either the token account or the controlling wallet is in the list
+                entry.token_account == destination_token_key
+                 || entry.wallet_account == owner_wallet_key
+            });
+
+        if !is_whitelisted {
+            panic!("Neither the token account nor the wallet is in the whitelist!");
         }
 
-        msg!("Account in white list, all good!");
-
+        msg!("Destination is whitelisted (token or wallet), all good!");
         Ok(())
     }
-
-    pub fn add_to_whitelist(ctx: Context<AddToWhiteList>) -> Result<()> {
+    
+    pub fn add_to_whitelist(
+        ctx: Context<AddToWhiteList>,
+        wallet_account: Pubkey,   // new param for the controlling wallet
+    ) -> Result<()> {
+        // Ensure only the authority can add
         if ctx.accounts.white_list.authority != ctx.accounts.signer.key() {
-            panic!("Only the authority can add to the white list!");
+            return err!(TransferError::NotWhitelistAuthority);
         }
 
-        ctx.accounts
-            .white_list
-            .white_list
-            .push(ctx.accounts.new_account.key());
+        let token_account_key = ctx.accounts.new_account.key();
+
+        // Check if this (token_account, wallet_account) is already in the list
+        let already_in_list = ctx.accounts.white_list.white_list.iter().any(|entry| {
+            entry.token_account == token_account_key && entry.wallet_account == wallet_account
+        });
+        if already_in_list {
+            return err!(TransferError::WalletAlreadyWhitelisted);
+        }
+
+        // If not present, push a new WhitelistEntry
+        let entry = WhitelistEntry {
+            token_account: token_account_key,
+            wallet_account,
+        };
+        ctx.accounts.white_list.white_list.push(entry);
+
         msg!(
-            "New account white listed! {0}",
-            ctx.accounts.new_account.key().to_string()
+            "New account whitelisted! token={}, wallet={}",
+            token_account_key,
+            wallet_account
         );
         msg!(
-            "White list length! {0}",
+            "White list length = {}",
             ctx.accounts.white_list.white_list.len()
         );
 
         Ok(())
     }
+    // pub fn remove_from_whitelist(
+    //     ctx: Context<RemoveFromWhiteList>,
+    //     wallet_account: Pubkey,
+    // ) -> Result<()> {
+    //     if ctx.accounts.white_list.authority != ctx.accounts.signer.key() {
+    //         panic!("Only the authority can remove from the white list!");
+    //     }
+    
+    //     let token_account_key = ctx.accounts.account_to_remove.key();
+    //     let wl = &mut ctx.accounts.white_list.white_list;
+    
+    //     // Retain everything that does NOT match both the token & wallet
+    //     wl.retain(|entry| {
+    //         !(entry.token_account == token_account_key
+    //           && entry.wallet_account == wallet_account)
+    //     });
+    
+    //     msg!("Removed token={}, wallet={} from whitelist (if present).", token_account_key, wallet_account);
+    //     msg!("Whitelist length = {}", wl.len());
+    //     Ok(())
+    // }
+    pub fn remove_from_whitelist(
+        ctx: Context<RemoveFromWhiteList>,
+        wallet_account: Pubkey, // controlling wallet
+    ) -> Result<()> {
+        // Ensure only the authority can remove
+        if ctx.accounts.white_list.authority != ctx.accounts.signer.key() {
+            return err!(TransferError::NotWhitelistAuthority);
+        }
+
+        let token_account_key = ctx.accounts.account_to_remove.key();
+        let wl = &mut ctx.accounts.white_list.white_list;
+
+        // Retain everything that does NOT match (token_account, wallet_account)
+        let old_len = wl.len();
+        wl.retain(|entry| {
+            !(
+                entry.token_account == token_account_key 
+                && entry.wallet_account == wallet_account
+            )
+        });
+        let new_len = wl.len();
+
+        msg!("Removed token={}, wallet={} from whitelist (if present). Old len={}, new len={}",
+            token_account_key, wallet_account, old_len, new_len
+        );
+        Ok(())
+    }
+    
 }
 
 fn check_is_transferring(ctx: &Context<TransferHook>) -> Result<()> {
@@ -170,8 +268,29 @@ pub struct AddToWhiteList<'info> {
     pub signer: Signer<'info>,
 }
 
+#[derive(Accounts)]
+pub struct RemoveFromWhiteList<'info> {
+    /// The token account you want to remove
+    /// (and we might also need the wallet key as an arg too)
+    /// /// CHECK: account_to_remove is a token account
+    #[account()]
+    pub account_to_remove: AccountInfo<'info>,
+
+    #[account(mut)]
+    pub white_list: Account<'info, WhiteList>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+}
+
+
 #[account]
 pub struct WhiteList {
     pub authority: Pubkey,
-    pub white_list: Vec<Pubkey>,
+    pub white_list: Vec<WhitelistEntry>,
+}
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Debug)]
+pub struct WhitelistEntry {
+    pub token_account: Pubkey,
+    pub wallet_account: Pubkey,
 }
